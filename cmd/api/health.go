12c1bbd8e7cbe249779205edd6740686bda244bf
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-05 12:46:26 +0530

    fixed small bugs and added main.go code for running

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]string{
 		"status": "ok",
 		"env":    app.conf.env,
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,28 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Println("Hello World")
+	"github.com/DivyanshuShekhar55/go-rss/internal/env"
+	"go.uber.org/zap"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("PORT", ":8080"),
+		env:  env.GetString("ENV", "dev"),
+	}
+
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	app := &application{
+		conf:   cfg,
+		logger: logger,
+	}
+
+	mux := app.mount()
+	fmt.Println("running")
+	logger.Fatal(app.run(mux))
+
 }
