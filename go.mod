941a9a0fe751942e9903a34bcc83a9abb7a8997e
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-07 13:21:02 +0530

    feat : added bcrypt functionalities and the library

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 )
--- internal/store/users.go
+++ internal/store/users.go
@@ -1,14 +1,30 @@
 package store
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
-	ID int64 `json:"id"`
-	Username string `json:"username"`
-	Email string `json:"email"`
-	Password password `json:"-"`
-	CreatedAt string `json:"created_at"`
+	ID        int64    `json:"id"`
+	Username  string   `json:"username"`
+	Email     string   `json:"email"`
+	Password  password `json:"-"`
+	CreatedAt string   `json:"created_at"`
 }
 
 type password struct {
 	text *string
 	hash []byte
+}
+
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	p.text = &text
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Compare(text string) error {
+	return bcrypt.CompareHashAndPassword(p.hash, []byte(text))
 }
