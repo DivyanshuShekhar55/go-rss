f21004094e837f0cb5896ea86d30661dc76ce069
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-09 11:56:26 +0530

    added creating user function in users.go and added hashing of password

--- internal/store/users.go
+++ internal/store/users.go
@@ -1,7 +1,13 @@
 package store
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"context"
+	"database/sql"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
 
+// NOTE : the following is not a SQL structure, it is a Go struct for encoding/decoding json data
 type User struct {
 	ID        int64    `json:"id"`
 	Username  string   `json:"username"`
@@ -10,6 +16,7 @@ type User struct {
 	CreatedAt string   `json:"created_at"`
 }
 
+// text is un-hashed password
 type password struct {
 	text *string
 	hash []byte
@@ -26,5 +33,37 @@ func (p *password) Set(text string) error {
 }
 
 func (p *password) Compare(text string) error {
-	return bcrypt.CompareHashAndPassword(p.hash, []byte(text))
+	return bcrypt.CompareHashAndPassword(p.
+		hash, []byte(text))
 }
+
+type UserStore struct {
+	db *sql.DB
+}
+
+func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
+	query := `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id, created_at`
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	err := tx.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password.hash,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		switch {
+		// check for errors from pgx driver and do it
+		}
+	}
+	return nil
+}
+
+
