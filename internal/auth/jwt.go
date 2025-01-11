6fcc09bac118761c6b11f2d0b74510913051887f
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-11 11:40:03 +0530

    feat : added the jwt.go file with generation and validation methods for tokens

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,2 +1,50 @@
 package auth
 
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTAuthenticator struct {
+	secret string
+	aud    string
+	iss    string
+}
+
+func NewJWTAuthenticator(secret, aud, iss string) *JWTAuthenticator {
+	return &JWTAuthenticator{secret, iss, aud}
+}
+
+func (a *JWTAuthenticator) GenerateToken(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(a.secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+func (a *JWTAuthenticator) ValidateToken(token string) (*jwt.Token, error) {
+
+	// firstly parse the token, i.e, break into header, payload and signature
+	return jwt.Parse(token, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method %v", t.Header["alg"])
+		}
+		return []byte(a.secret), nil
+	},
+
+		// after checking proper signature above, next we check
+		// the expiration date validation, audience and issuer validation too
+		jwt.WithExpirationRequired(),
+		jwt.WithAudience(a.aud),
+		jwt.WithIssuer(a.iss),
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Name}),
+	)
+
+}
