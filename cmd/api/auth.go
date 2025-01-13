4f42c540a07169ce5ce53552f53d0c64352433d4
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-13 09:47:48 +0530

    feat : added signatures for auth handlers of token and user (register user)

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,10 +1,17 @@
-package main 
+package main
 
-type RegisteredUserPayload struct{
-    Username string `json:"username"`
-    Email string `json:"email"`
-    Password string `json:"password"`
+import "net/http"
+
+type RegisteredUserPayload struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
 
+}
 
+func (app *application) createTokenHandler(w http.ResponseWriter, r *http.Request) {
+
+}
