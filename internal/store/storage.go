5042eff4f3ea329d2cfd68ff1bce34513e722e76
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-13 20:29:12 +0530

    feat : added variables for error and query-timeouts

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,11 +3,19 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound          = errors.New("resource Not Found")
+	QueryTimeoutDuration = time.Second * 5
+	ErrConflict          = errors.New("resource already exists")
 )
 
 type Storage struct {
 	Users interface {
-		GetByID(context.Context,int64) (*User, error)
+		GetByID(context.Context, int64) (*User, error)
 		Create(context.Context, *sql.Tx, *User) (*User, error)
 		Delete(context.Context, int64) error
 	}
