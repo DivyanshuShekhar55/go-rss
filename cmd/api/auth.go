eaa4b21e0bc075346895fda3fb5b4d5279d406aa
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-13 19:38:20 +0530

    added comments for user password hashing and setting

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -3,8 +3,8 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/DivyanshuShekhar55/go-rss/internal/store"
+	"github.com/go-playground/validator/v10"
 )
 
 var validate *validator.Validate
@@ -19,13 +19,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	validate = validator.New()
 
 	var payload RegisteredUserPayload
-	if err := readJSON(w, r, &payload); err !=nil{
+	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
 	// do some validation checks
-	if err:=validate.Struct(payload); err!=nil{
+	if err := validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
@@ -33,17 +33,31 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	// user has been validated, all good in struct, can now try to put to db
 	user := &store.User{
 		Username: payload.Username,
-		Email:payload.Email,
+		Email:    payload.Email,
 		// put the password after hashing it
 	}
-	
-	if err:= user.Password.Set(payload.Password); err != nil {
+
+	// HASHING THE PASSWORD
+	// type User struct {
+	// 	ID        int64    `json:"id"`
+	// 	Username  string   `json:"username"`
+	// 	Email     string   `json:"email"`
+	// 	Password  password `json:"-"`
+	// 	CreatedAt string   `json:"created_at"`
+	// }
+	// here is how the user struct looks like and note that the passwords struct has a Set function (refer to users.go file in internals/store)
+	// the user var created locally here, will get the password field set as user.Password = ""...
+	// but rather we are saying the password will pass through the Set function and then be set ... basically calling function on a struct field
+
+	if err := user.Password.Set(payload.Password); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
 
-	
+	ctx := r.Context()
 
+	// generate the token, here if you want to have the functionality to activate the user via email sending
+	// put functionality of email, sending here if you want it
 
 }
 
