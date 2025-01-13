248185f35a7683f9be900dbd390e7297ea940a80
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-13 09:00:33 +0530

    feat : added func signatures for handling auth feats

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,1 +1,10 @@
 package main 
+
+type RegisteredUserPayload struct{
+    Username string `json:"username"`
+    Email string `json:"email"`
+    Password string `json:"password"`
+}
+
+
+
--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,2 +1,11 @@
-package main 
+package main
 
+import "net/http"
+
+func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request){
+
+}
+
+func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Request){
+	
+}
