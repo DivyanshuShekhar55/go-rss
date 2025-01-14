1ae3a6319b886d5019422b3016071b0a5013c9ef
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-14 12:06:31 +0530

    added the transactions wrapper function

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -20,3 +20,23 @@ type Storage struct {
 		Delete(context.Context, int64) error
 	}
 }
+
+// create a function that takes in a func 'fn' as an input (update, delete, create)
+// tries to perform fn with the transaction mechanism - if fails all changes made are rolled-back
+func withTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// apply the function ...
+	if err := fn(tx); err != nil {
+
+		//if err undo all changes
+		_ = tx.Rollback()
+		return err
+	}
+
+	// if no error actually change the db
+	return tx.Commit()
+}
