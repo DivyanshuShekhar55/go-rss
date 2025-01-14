99874e9e9733e27a155ac33d4d6c22c4979c2eff
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-14 09:00:35 +0530

    finished with token generation part when logging in

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/DivyanshuShekhar55/go-rss/internal/store"
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var validate *validator.Validate
@@ -81,11 +83,53 @@ func (app *application) createTokenHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	// validate against the struct whether it is properly "formatted"
-	if err:= validate.Struct(payload); err != nil{
+	if err := validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
 	// the user payload is fine, can procced to check the user details against the db details
+	// ideally it should be getUserByEmail as the user will put his/her mail only in frontend
+	user, err := app.store.Users.GetByID(r.Context(), payload.ID)
+	if err != nil {
+		switch err {
+		case store.ErrNotFound:
+			app.unauthorisedErrorResponse(w, r, err)
+		default:
+			app.internalServerError(w, r, err)
+		}
+		return
+	}
+
+	// user with the given id found
+	// proceed to compare password
+	if err := user.Password.Compare(payload.Password); err != nil {
+		app.unauthorisedErrorResponse(w, r, err)
+		return
+	}
+
+	// password also correct, move to creating new jwt token
+	claims := jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(app.conf.auth.token.exp).Unix(),
+		"iat": time.Now().Unix(),
+		"nbf": time.Now().Unix(),
+		"iss": app.conf.auth.token.iss,
+
+		// *** HAVE A DOUBT ON THE FOLLOWING PART .iss or .aud ?? ***
+		"aud": app.conf.auth.token.iss,
+	}
+
+	// jwt has been structured, now generate it
+	token, err := app.autheticator.GenerateToken(claims)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	// token generated, send response
+	if err := app.jsonResponse(w, token, http.StatusCreated); err != nil {
+		app.internalServerError(w, r, err)
+	}
 
 }
